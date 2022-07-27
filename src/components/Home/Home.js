ed5f8e237d890d3c384f4d5fa1add429e95d43a1
JoaoSGabriel <joaogabriel0359@gmail.com>
2022-07-27 10:41:26 -0300

    link API movies_Theaters

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,36 +1,36 @@
 import { Link } from "react-router-dom";
+import React from "react";
+import { useEffect } from "react";
+import axios from 'axios';
 import "./style.css";
 
-function Movies () {
+function Movies (props) {
+    const {posterURL} = props;
     return(
         <Link to="/filme">
             <div className="movie-Background">
-                <img src="https://br.web.img2.acsta.net/pictures/20/08/18/16/25/0872062.jpg" alt="coverpage" />
+                <img src={posterURL} alt="coverpage" />
             </div>
         </Link>
     );
 }
 
 export default function Home () {
+    const [movies_Theaters, setMovies_Theaters] = React.useState([]);
+
+    useEffect(() => {
+        const promisse = axios.get("https://mock-api.driven.com.br/api/v5/cineflex/movies");
+
+        promisse.then(answer => {
+            setMovies_Theaters(answer.data);
+        });
+    }, []);
+
     return(
         <>
             <div className="title"><h1>Selecione o filme</h1></div>
             <div className="movies">
-                <div className="left-Movies">
-                    <Movies />
-                    <Movies />
-                    <Movies />
-                    <Movies />
-                    <Movies />
-                </div>
-                <div className="rigth-Movies">
-                    <Movies />
-                    <Movies />
-                    <Movies />
-                    <Movies />
-                    <Movies />
-                    <Movies />
-                </div>
+                {movies_Theaters.map(item => <Movies posterURL={item.posterURL}/>)}
             </div>
         </>
     );
